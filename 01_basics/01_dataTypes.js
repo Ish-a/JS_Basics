dc982808e551722cdf88f4427ea046fefb0b2f55
Anushka Tiwari <104916962+Ish-a@users.noreply.github.com>
2025-01-15 13:19:14 +0000

    variables and datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //it treats all js code in newer version
+
+// alert(3+3) This is not in practice in node environment. Gives error
+
+console.log(3
+    +
+    3
+) //code readibility should be high
+
+console.log("Anushka") //not using semicolon as it is using space
+
+// datatypes used 
+let name = "anushka"
+let age = 21
+let isLoggedIn = false
+
+//number => 2^53
+//biginit
+//string =>""
+//boolean => true/false
+//null => standalone value (representation of an empty value)
+//undefined => value not assigned yet
+//symbol => unique
+
+//object
+
+// typeof null -> object as an output
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountNumber = 987654321
+const accountName = "Anushka Tiwari"
+let accountCity = "Sikkim"
+var accountEmail = "anushka@google.com"
+accountAmount = 923887
+
+
+
+console.table([accountCity,accountEmail,accountName,accountNumber,accountAmount])
