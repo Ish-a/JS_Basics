5036d55fdf65052eb9508c6fc07201437ffd2b97
Anushka Tiwari <104916962+Ish-a@users.noreply.github.com>
2025-01-16 13:01:11 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -28,3 +28,47 @@ let someNum = 88
 let stringNumber = String(someNum)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// ------OPERATIONS------
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) //power
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Anushka"
+
+console.log(str1+str2);
+
+// console.log("1" + 2) //12
+// console.log(1 + "2") //12
+// console.log("1" + 2 + 2) //122
+// console.log(1 + 2 + "2") //32
+
+console.log(true);
+console.log(+true); //1
+// console.log(true+); //error
+console.log(+""); //0
+
+let gameCounter = 100
+gameCounter++
+// ++gameCounter //same as gameCounter++
+// console.log(gameCounter)
+
+// ------Example from mdn docs
+
+let x = 3;
+let y = x++ //assign hone ke baad increment hua x, so y = 3
+console.log(x,y)
+
+let a = 4
+let b = ++a //assign hone ke phele he a increment ho gaya
+console.log(a,b)
+
