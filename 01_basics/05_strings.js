a42b673abe9ce89c130c5588b33c6b736014260c
Anushka Tiwari <104916962+Ish-a@users.noreply.github.com>
2025-01-17 11:57:13 +0000

    strings done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -18,4 +18,25 @@ console.log(gameName.__proto__); //{} - functions that could be accessed
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2)); //u
 console.log(gameName.indexOf('t'));
+
+const subbedString = gameName.substring(0,4);
+console.log(subbedString)
+
+const slicedString = gameName.slice(-8,4) //could use negative
+console.log(slicedString)
+
+const newStringOne = "   isha    "
+
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+ const url = "https://anushka.com/anuhska tiwari"
+ console.log(url.replace('%20','-'))
+
+ //browser converts empty spaces into %20
+
+ console.log(url.includes('anushka'))
+
+ console.log(gameName.split('-'));
  
